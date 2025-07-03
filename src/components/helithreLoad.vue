25c9e2e0bd23e9d36425e9bf26bfc479106ec810
Lorenzo cirelli <88227516+LorenzoCirelli@users.noreply.github.com>
2025-07-03 11:34:50 +0200

    Better typization and fallback

--- src/components/helithreLoad.vue
+++ src/components/helithreLoad.vue
@@ -1,36 +1,37 @@
 <template>
-    <component :is="wrapperInUse.compType" :childrens="wrapperInUse.compProps" :name="helithreJson.name"></component>
+  <component
+    :is="wrapperInUse.compType"
+    :childrens="wrapperInUse.compProps"
+    :name="helithreJson.name"
+  />
 </template>
+
 <script lang="ts" setup>
-import { defineComponent, type Component, type PropType } from 'vue';
+import { type Component } from 'vue';
 import type { FormHelitreJSON } from '../types/helitreJSON';
 import { BasicWrapperTypeEnum } from '../types/wrapper';
 import { BasicWrapper, SaveWrapper } from '../composable/wrapper/basicWrapper';
 import { wrapper } from '../index';
-const props = defineProps({
-    helithreJson: {
-        type: Object as PropType<FormHelitreJSON>,
-        required: true
-    }
-})
 
-let wrapperInUse: {
-    compType: Component,
-    compProps: SaveWrapper | BasicWrapper | null
-}= {
-    compType: defineComponent,
-    compProps: null
-}
+const props = defineProps<{
+  helithreJson: FormHelitreJSON
+}>();
 
-//check if the wrapper type exist and create the correct object to manage it
-if (props.helithreJson.wrapper.trim() == BasicWrapperTypeEnum.form) {
-    wrapperInUse.compProps = new SaveWrapper(props.helithreJson.childrens);
-    wrapperInUse.compType = wrapper.HelithreForm;
-} else if (props.helithreJson.wrapper.trim() == BasicWrapperTypeEnum.page) {
-    wrapperInUse.compProps = new BasicWrapper(props.helithreJson.childrens);
-     wrapperInUse.compType = wrapper.HelithrePage;
+const wrapperInUse: {
+  compType: Component,
+  compProps: SaveWrapper | BasicWrapper | null
+} = {
+  compType: wrapper.HelithrePage,
+  compProps: null
+};
+
+if (props.helithreJson.wrapper.trim() === BasicWrapperTypeEnum.form) {
+  wrapperInUse.compProps = new SaveWrapper(props.helithreJson.childrens);
+  wrapperInUse.compType = wrapper.HelithreForm;
+} else if (props.helithreJson.wrapper.trim() === BasicWrapperTypeEnum.page) {
+  wrapperInUse.compProps = new BasicWrapper(props.helithreJson.childrens);
+  wrapperInUse.compType = wrapper.HelithrePage;
 } else {
-    throw new Error(`The wrapper type is not page or form, ${props.helithreJson.wrapper.trim()} passed`);
+  throw new Error(`Invalid wrapper: ${props.helithreJson.wrapper}`);
 }
-
 </script>
